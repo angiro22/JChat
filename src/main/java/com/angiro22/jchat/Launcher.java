e1bf9abd84b2bdf708f800b094feeab04f0b93c0
angiro22 <andreagirone2710@gmail.com>
2026-09-18 14:07:55 +0200

    fix: remove useless file

--- src/main/java/com/angiro22/jchat/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.angiro22.jchat;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
