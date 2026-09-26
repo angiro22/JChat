004f44c0972c60d2baef6d24b2751917b30e7bab
angiro22 <andreagirone2710@gmail.com>
2026-09-26 16:28:54 +0200

    feat: define Protocol class

---
+++ src/main/java/com/jchat/common/Protocol.java
@@ -0,0 +1,11 @@
+package com.jchat.common;
+
+public final class Protocol {
+    final public static String HOST = "127.0.0.1",
+            SEPARATOR = ";",
+            EXIT_COMMAND = "/exit";
+    final public static int PORT = 3333;
+
+    private Protocol() {
+    }
+}
