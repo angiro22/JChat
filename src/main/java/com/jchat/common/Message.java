8074f468d4b342f9c66a058a498a0c4fbdaac954
angiro22 <andreagirone2710@gmail.com>
2026-09-26 16:53:42 +0200

    feat: define a method that given the string, parse it into a Message object

--- src/main/java/com/jchat/common/Message.java
+++ src/main/java/com/jchat/common/Message.java
@@ -12,4 +12,14 @@ public class Message {
     public String toString() {
         return username + Protocol.SEPARATOR + " " + userMessage;
     }
+
+    // Given a message, this method parse it to a Message object
+    public static Message StringToMessage(String messageReceived) {
+        String username, text;
+        username = messageReceived.substring(0, messageReceived.indexOf(Protocol.SEPARATOR));
+        // Use separator + 2 for the text index because in toString() the text is sent with a space after the separator
+        text = messageReceived.substring(messageReceived.indexOf(Protocol.SEPARATOR) + 2);
+
+        return new Message(text, username); 
+    }
 }
